79e31066b3da3dcb9501b45cc589ab7a04510b29
MIHIR PATEL <138042700+Mihiiirpatel@users.noreply.github.com>
2026-08-12 20:50:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (30.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,10 +4,8 @@ public:
         transform(s.begin(),s.end(),s.begin(),::tolower);
          string m;
         for(int i=0; i<s.length(); i++){
-            if(s[i]>='a' && s[i]<='z'){
+            if(s[i]>='a' && s[i]<='z' || s[i]>='0' && s[i]<='9'){
                   m+=s[i];
-            }else if(s[i]>='0' && s[i]<='9'){
-                m+=s[i];
             }
         }
         int lft=0,rht=m.length()-1;
