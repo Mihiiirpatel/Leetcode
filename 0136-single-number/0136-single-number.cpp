c114d077cfa58515612dde5b5fc8a280af374bf8
MIHIR PATEL <138042700+Mihiiirpatel@users.noreply.github.com>
2026-08-22 10:26:28 +0530

    Time: 11 ms (13.48%), Space: 20.8 MB (24.23%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -5,6 +5,7 @@ public:
         for(int i=0; i<nums.size()-1; i++){
                 if(nums[i]!=nums[i+1]){
                     return nums[i];
+                    break;
                 }else{
                     i++;
                 }
