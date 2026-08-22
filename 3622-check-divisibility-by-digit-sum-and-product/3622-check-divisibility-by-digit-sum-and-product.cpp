cb33e30c4daac4657e61788242c6740dc25f41c4
MIHIR PATEL <138042700+Mihiiirpatel@users.noreply.github.com>
2026-08-22 11:00:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.55%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool checkDivisibility(int n) {
-        int sm=0,mlti=1;
+        int sm=0,mlti=1,rem;
         for(int i=n; i>0; i/=10){
-            int rem=i%10;
+            rem=i%10;
             sm+=rem;
             mlti*=rem;
         }
