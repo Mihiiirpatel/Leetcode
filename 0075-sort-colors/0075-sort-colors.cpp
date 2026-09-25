031510ded5f49907738814ae0b7103cf229c2ac2
MIHIR PATEL <138042700+Mihiiirpatel@users.noreply.github.com>
2026-09-25 22:43:26 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,9 +3,9 @@ public:
     void sortColors(vector<int>& nums) {
       int n=nums.size();
       for(int i=0 ;i<n ;i++){
-        for(int j=0; j<n; j++){
-            if(nums[i]<nums[j]){
-                swap(nums[i],nums[j]);
+        for(int j=0; j<n-1; j++){
+            if(nums[j]>nums[j+1]){
+                swap(nums[j],nums[j+1]);
             }
         }
       }
